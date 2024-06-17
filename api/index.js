e5dffbffc18d90e0ff6c72f21523f569e1d3a3de
abi-widiarta <abiwidiarta03@gmail.com>
2024-06-17 21:12:02 +0700

    fix: move index.js to api folder

--- api/index.js
+++ api/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const route = require("./routes/products.route.js");
+const route = require("../routes/products.route.js");
 
 const app = express();
 
@@ -17,7 +17,6 @@ app.get("/", (req, res) => {
 try {
   mongoose.connect("mongodb+srv://abiwidiarta03:aMzGXwx5zGmQtlAn@backendnode.4jit2jp.mongodb.net/Node-API?retryWrites=true&w=majority&appName=BackEndNode").then(() => {
     console.log("Database Connected");
-    app.listen(3000, () => console.log("server started on port 3000"));
   });
 } catch (error) {
   console.log("Failed to connect to database : ", error);
